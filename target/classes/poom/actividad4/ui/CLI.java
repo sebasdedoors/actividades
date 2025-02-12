47af93e26c85d98096d2428d70f695d13afe2eba
danielaliz1 <daniela.liz.real@outlook.com>
2025-02-11 19:10:28 -0600

    Se guardo el CLI correctamente

--- target/classes/poom/actividad4/ui/CLI.java
+++ target/classes/poom/actividad4/ui/CLI.java
@@ -1,3 +1,17 @@
+package poom.actividad4.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
 public class CLI {
+
+    public static void main(String[] args) {
+        
+    }
+
+
 
 }

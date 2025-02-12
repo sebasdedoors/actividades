e9eab90d01c43933c0dd9903b8cd598b66260e43
Victor-Ibarra-Ortiz <ibarraortizvictor@gmail.com>
2025-02-12 11:03:44 -0600

    Se actualizo la clase de Subject :D

--- src/main/java/poom/actividad4/models/Subject.java
+++ src/main/java/poom/actividad4/models/Subject.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 public class Subject {
   
-    public String name;
-    public int credits;
-    public String ID;
-    public Course course;
-    public List<Student> students;
-    public Teacher teacher;
+    private String name;
+    private int credits;
+    private String ID;
+    private String course;
+    private List<Student> students;
+    private Teacher teacher;
+    private int hours;
 
+    @Override
     public String toString() {
         return "Materia{" +
         "nombre='" + name + '\'' +
@@ -19,14 +21,73 @@ public class Subject {
         '}';
     }
 
-public Subject(String nombre, int creditos, String ID, int horas) {
+public Subject(String name, int credits, String ID, String course, int hours, Teacher teacher){
     this.name = name;
     this.credits = credits;
     this.ID = ID;
     this.hours = hours;
+    this.course = course;
+    this.teacher = teacher;
+    setName(name);
+    setCredits(credits);
+    setID(ID);
+    setHours(hours);
+    setCourse(course);
+    setTeacher(teacher);
+    
     this.students = new ArrayList<>();
    }
-public  int hours;
-
- }
-
+public String getName(){
+    return name;
+}
+public int getCredits(){
+    return credits;
+}
+public String getID(){
+    return ID;
+}
+public  int getHours(){
+    return hours;
+}
+public String getCourse(){
+    return course;
+}
+public Teacher getTeacher(){
+    return teacher;
+}
+public void setName(String name){
+    if (name == null || name.trim().isEmpty() || name.matches(".*\\d.*")) {
+        throw new IllegalArgumentException("El nombre no puede contener números.");
+    }
+    this.name = name;
+}
+public void setCredits(int credits){
+    if (credits == 0){
+        throw new IllegalArgumentException("Los créditos deben de tener un mínimo de 1");
+    }
+    this.credits = credits;
+}
+public void setID(String ID){
+    if (ID == null){
+        throw new IllegalArgumentException("El esapcio no puede estar vacio. ");
+    }
+}
+public void setHours(int hours){
+    if (hours < 0){
+        throw new IllegalArgumentException("Las horas no pueden ser negativas.");
+    }
+    this.hours = hours;
+}    
+    public void setCourse(String course){
+        if(course == null || course.trim().isEmpty() || course.matches(".*\\d.*")){
+            throw new IllegalArgumentException("El curso no puede contener números.");
+        }
+        this.course = course;
+    }
+public void setTeacher(Teacher teacher){
+    if (teacher == null){
+        throw new IllegalArgumentException("El profesor no puede ser nulo.");
+    }
+    this.teacher = teacher;
+    }
+}
